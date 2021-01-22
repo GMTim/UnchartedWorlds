bfa6728f769b005675d3472da27be7d11ec65d3f
Timothy Rascher <timrascher@gmail.com>
2021-01-22 15:02:17 -0800

    Added in origin data.

--- javascript/characterCreation/careers.js
+++ javascript/characterCreation/careers.js
@@ -11,11 +11,6 @@ export default class {
             second = Common.random(this.careers)
         } while (second == first)
         let items = Common.sort([first, second], "name")
-        // items.sort((a, b) => {
-        //     if (a.name < b.name) { return -1 }
-        //     if (a.name > b.name) { return 1 }
-        //     return 0
-        // })
         return items
     }
 }
--- javascript/characterCreation/character.js
+++ javascript/characterCreation/character.js
@@ -1,4 +1,3 @@
-import Common from "../common.js"
 import Careers from "./careers.js"
 import Origins from "./origins.js"
 
@@ -26,6 +25,8 @@ export default class {
          parent.append(ele("Origin: " + this.origin.name))
          parent.append(ele("Career One: " + this.careers[0].name))
          parent.append(ele("Career Two: " + this.careers[1].name))
+         parent.append($("<br />"))
+         parent.append(ele(JSON.stringify(this.origin)))
          parent.append($("<br />"))
          parent.append(ele(JSON.stringify(this.careers[0])))
          parent.append($("<br />"))
