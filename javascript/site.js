de9602a9529cd41ad9145766173dd1c935e27d2f
Timothy Rascher <timrascher@gmail.com>
2021-01-22 15:59:40 -0800

    Added in Origin Selector.

--- javascript/site.js
+++ javascript/site.js
@@ -1,9 +1,12 @@
 import Character from "./characterCreation/character.js"
+import OriginSelector from "./elements/originSelector.js"
 
 const character = new Character()
+const originSelector = new OriginSelector(character)
 
 $(async () => {
     await character.load()
-    character.generate()
-    character.map()
+    originSelector.load()
+    // character.generate()
+    // character.map()
 })
